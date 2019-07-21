8916ee5d3603dde73a3dcc32a1b7a84facb72ea0
Calle <36276161+qalle-fivem@users.noreply.github.com>
2019-07-21 15:21:25 +0200

    updated sql.

--- pear_character.sql
+++ pear_character.sql
@@ -2,18 +2,16 @@ USE `pear`;
 
 -- Dumpar struktur för tabell pear.users_characters
 CREATE TABLE IF NOT EXISTS `users_characters` (
+  `steamHex` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `characterId` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `firstname` varchar(150) COLLATE utf8mb4_bin NOT NULL,
   `lastname` varchar(150) COLLATE utf8mb4_bin NOT NULL,
   `dateofbirth` varchar(150) COLLATE utf8mb4_bin NOT NULL,
   `lastdigits` int(4) NOT NULL DEFAULT '0',
+  `cash` bigint(20) NOT NULL DEFAULT '0',
+  `bank` bigint(20) NOT NULL DEFAULT '0',
+  `job` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT 'unemployed',
+  `timeCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`characterId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Dumpar data för tabell pear.users_characters: ~0 rows (ungefär)
-/*!40000 ALTER TABLE `users_characters` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users_characters` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
